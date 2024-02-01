a775194103c495563ccdf43fab3554a3e8112b06
berezhnoy <bi068612@gmail.com>
2024-02-01 20:34:32 +0300

    Большая задача по Maps API. Часть №2

--- MainWindow.py
+++ MainWindow.py
@@ -1,5 +1,6 @@
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import Qt
 from map_api_form import Ui_MainWindow
 import requests
 from io import BytesIO
@@ -13,22 +14,17 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.init_UI()
 
     def init_UI(self):
-        response = self.get_map()
+        self.ll = list(map(float, input().split()))
+        self.scale = float(input())
+        self.MIN_SCALE = 0.0005
+        self.MAX_SCALE = 0.1
 
-        img = response.content
-
-        pixmap = QPixmap()
-        pixmap.loadFromData(img)
-
-        self.label.setPixmap(pixmap)
+        self.update_map()
 
     def get_map(self):
-        ll = input().split()
-        scale = input()
-
         map_params = {
-            "ll": ",".join(ll),
-            "spn": ",".join([scale, scale]),
+            "ll": ",".join(map(str, self.ll)),
+            "spn": ",".join(map(str, (self.scale, self.scale))),
             "l": "map"
         }
 
@@ -37,5 +33,30 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         response = requests.get(map_api_server, params=map_params)
 
         return response
+
+    def update_map(self):
+        response = self.get_map()
+        if response:
+            img = response.content
+
+            pixmap = QPixmap()
+            pixmap.loadFromData(img)
+
+            self.label.setPixmap(pixmap)
+
+    def set_scale(self, scale):
+        self.scale = max(self.MIN_SCALE, min(self.MAX_SCALE, scale))
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            previous = self.scale
+            self.set_scale(self.scale + 0.0005)
+            if self.scale != previous:
+                self.update_map()
+        elif event.key() == Qt.Key_PageDown:
+            previous = self.scale
+            self.set_scale(self.scale - 0.0005)
+            if self.scale != previous:
+                self.update_map()
 
 
